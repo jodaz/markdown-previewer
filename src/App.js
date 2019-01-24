8a4404e675e80bd4f97c705e3798068ad0ae629e
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-01-24 12:04:19 -0400

    Create function which convert markdown to html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import marked from 'marked';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+
+  handleChange(event) {
+    this.setState({
+      value: event.target.value,
+    });
+  }
+  
+  getRawHtml() {
+    let rawHtml = marked(this.state.value, {sanitize: true});
+    return { __html: rawHtml };
+  }
+
   render() {
     return (
       <div className="App">
-        <p>Hello World!</p>
+        <textarea id="markdown" value={this.state.value} onChange={this.handleChange}></textarea>
+        <div id="preview" dangerouslySetInnerHTML={this.getRawHtml()}></div>
       </div>
     );
   }

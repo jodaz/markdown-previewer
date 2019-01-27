9c72a8494b05639fecced9de00916a67ff0d721a
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-01-27 19:20:50 -0400

    Add bootstrap. Refactorize App's components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,83 @@ import React, { Component } from 'react';
 import './App.css';
 import marked from 'marked';
 
+const placeholder = `
+# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+  
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+  
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want! 
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`
+
+const Editor = (props) => {
+  return(
+    <div className="col-md-6">
+      <div className="container">
+        <textarea id="editor"
+          value={props.value}
+          handleChange={props.handleChange}
+        />
+      </div>
+    </div>
+  );
+};
+
+const Previewer = (props) => {
+  return(
+    <div className="col-md-6">
+      <div id="preview" dangerouslySetInnerHTML={props.data}>
+      </div>
+    </div>
+  );
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {value: placeholder};
 
     this.handleChange = this.handleChange.bind(this);
   }
-
 
   handleChange(event) {
     this.setState({
@@ -24,12 +93,14 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <textarea id="markdown" value={this.state.value} onChange={this.handleChange}></textarea>
-        <div id="preview" dangerouslySetInnerHTML={this.getRawHtml()}></div>
+      <div className="App container">
+        <div className="row">
+          <Editor value={this.state.value} handleChange={this.state.handleChange} />
+          <Previewer data={this.getRawHtml()} />
+        </div>
       </div>
     );
   }
-}
+};
 
 export default App;

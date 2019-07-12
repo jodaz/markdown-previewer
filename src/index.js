f96505aeb702a2fce9b404030a6548dc1bfd1c73
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-07-12 08:56:35 -0400

    Modularize components and styles

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import AppHeader from './components/AppHeader/AppHeader';
+import Editor from './components/Editor/Editor';
+import Previewer from './components/Previewer/Previewer';
+import AppFooter from './components/AppFooter/AppFooter';
 
 // Import frontend, static files...
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Provider } from 'react-redux';
 import configureStore from './store';
+
+const App = () => {
+  return (
+    <div className="container h-100">
+      <AppHeader />
+      <div className="row h-75 align-items-center">
+        <Editor />
+        <Previewer />
+      </div>
+      <AppFooter />
+    </div>
+  );
+}
 
 ReactDOM.render(
   <Provider store={configureStore()}>

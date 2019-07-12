eae96892c779181956724cac2ab4f0fcb6abb50b
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-07-12 15:28:15 -0400

    Comment redux-devtools

--- src/store.js
+++ src/store.js
@@ -6,8 +6,11 @@ export default function configureStore() {
   return createStore(
     rootReducer,
     compose(
+      applyMiddleware(thunk)
+      /* Apply after updating redux-devtools extension package
       applyMiddleware(thunk),
       window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+      */
     )
   );
 }

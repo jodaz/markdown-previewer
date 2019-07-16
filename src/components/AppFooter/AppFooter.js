553f0ea088b4c9624f46ffc600d3c76fc2e76834
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-07-15 21:39:39 -0400

    Fix footer styles

--- src/components/AppFooter/AppFooter.js
+++ src/components/AppFooter/AppFooter.js
@@ -7,7 +7,8 @@ const AppFooter = () => {
     <footer className="col footer">
       <div className="text-center">
         <p>
-          <span>Made by</span><a href="http://jesuodz.github.io" target="_blank">Jesús Ordosgoitty</a>
+          <span>Made by</span>
+          <a href="http://jesuodz.github.io">Jesús Ordosgoitty</a>
         </p>
       </div>
     </footer>

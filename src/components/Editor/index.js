54809ab80e10e0832481aa633e50de96a1a18912
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-07-17 21:27:45 -0400

    Use TextArea component

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -1,51 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import TitleHeader from '../TitleHeader';
-import PropTypes from 'prop-types';
-
-import { connect } from 'react-redux';
-import { updatePlaceholder } from './actions';
+import TextArea from './components/TextArea';
 
 import './index.css'
 
-class Editor extends Component {
-  
-  handleChange(event) {
-    // Update placeholder
-    let input = event.target.value;
-    this.props.updatePlaceholder(input);
-  };
-
-  componentDidMount() {
-    // Load ls and update placeholder
-    if (localStorage.markdown) {
-      this.props.updatePlaceholder(localStorage.markdown);
-    }
-  }
-
-  render() {
-    // const maximized = this.props.maximized ? 'editor maximized' : 'editor';
-
-    return(
-      <div id="editor" className="col-md-6">
-        <TitleHeader name="editor"/>
-        <textarea className="editor"
-          value={this.props.rawText}
-          onChange={this.handleChange.bind(this)}
-        />
-      </div>
-    );
-  }
+const Editor = () => {
+  return(
+    <div id="editor" className="col-md-6">
+      <TitleHeader name="editor"/>
+      <TextArea />
+    </div>
+  );
 }
 
-Editor.propTypes = {
-  rawText: PropTypes.string.isRequired,
-  updatePlaceholder: PropTypes.func.isRequired,
-  maximized: PropTypes.bool.isRequired
-};
-
-const mapStateToProps = state => ({
-  rawText: state.text.placeholder,
-  maximized: state.toggle.titleHeader.editor
-});
-
-export default connect(mapStateToProps, { updatePlaceholder })(Editor);
+export default Editor;
